46a50350b5531f7d3ae89f8ea2a54749d158a88f
Gul Akberova <kbrovagul0@gmail.com>
2023-10-17 16:05:20 +0400

    gallery modal ios

--- src/galery/gallery.js
+++ src/galery/gallery.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", async function () {
   const urlParams = new URLSearchParams(window.location.search);
-  const id = urlParams.get("id");
+  const id = urlParams.get("Id");
   const idd = parseInt(id);
   // Bu, URL'den alınan id'yi konsolda gösterecek
 
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", async function () {
   async function fetchData() {
     try {
       const response = await fetch(
-        `https://cms.absherontm.az/api/admin/InfoRepoImage/Index?id=10`
+        `https://cms.absherontm.az/api/admin/inforepoimage/index?Id=25`
       );
       if (!response.ok) {
         throw new Error("API isteği başarısız oldu.");
@@ -32,16 +32,34 @@ document.addEventListener("DOMContentLoaded", async function () {
     data.forEach((item) => {
       const div = document.createElement("div");
       const image = document.createElement("img");
+
+      const firstLetter = item.imagePath ? item.imagePath[0].toLowerCase() : "";
+
+      // CSS sınıfını belirle
+      const cssClassG =
+        firstLetter === "r"
+          ? "ios_gallery_img"
+          : firstLetter === "a"
+          ? "android_gallery_img"
+          : "defaul_gallery_img";
+
       image.src = `https://cms.absherontm.az/uploads/images/${item.imagePath}`;
+      image.className = cssClassG;
       image.alt = item.name;
       div.appendChild(image);
       galleryDiv.appendChild(div);
 
+      const cssClass =
+        firstLetter === "i"
+          ? "ios_gallery_modal_img"
+          : firstLetter === "a"
+          ? "android_gallery_modal_img"
+          : "defaul_gallery_modal_img";
       // Div'e tıklama olayını ekleyin
       div.addEventListener("click", function () {
         modal.style.display = "block";
         modalImage.src = `https://cms.absherontm.az/uploads/images/${item.imagePath}`;
-
+        modalImage.className = cssClass;
         // Tarih dönüşümü
         const date = new Date(item.createdDate);
         const formattedDate = date.toLocaleDateString("en-US", {
